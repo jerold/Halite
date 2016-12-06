721f9463ca704f064b41ba788c12fb8ad475fb96
jerold <jerold.albertson@gmail.com>
2016-12-05 20:50:09 -0700

    Bot work

--- GoBot.go
+++ GoBot.go
@@ -12,25 +12,25 @@ import (
 
 const logFile = "goLog.txt"
 
-type stack struct {
+type Stack struct {
 	lock sync.Mutex // you don't have to do this if you don't want thread safety
 	s    []*Cell
 }
 
-func NewStack() *stack {
-	return &stack{sync.Mutex{}, make([]*Cell, 0)}
+func NewStack() *Stack {
+	return &Stack{sync.Mutex{}, make([]*Cell, 0)}
 }
 
-func (s *stack) Push(v *Cell) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+func (s *Stack) Push(v *Cell) {
+	// s.lock.Lock()
+	// defer s.lock.Unlock()
 
 	s.s = append(s.s, v)
 }
 
-func (s *stack) Pop() (*Cell, error) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+func (s *Stack) Pop() (*Cell, error) {
+	// s.lock.Lock()
+	// defer s.lock.Unlock()
 
 	l := len(s.s)
 	if l == 0 {
@@ -42,15 +42,15 @@ func (s *stack) Pop() (*Cell, error) {
 	return res, nil
 }
 
-func (s *stack) length() int {
+func (s *Stack) length() int {
 	return len(s.s)
 }
 
-func (s *stack) isEmpty() bool {
+func (s *Stack) isEmpty() bool {
 	return len(s.s) == 0
 }
 
-func (s *stack) isNotEmpty() bool {
+func (s *Stack) isNotEmpty() bool {
 	return !s.isEmpty()
 }
 
@@ -81,106 +81,175 @@ func opposite(direction hlt.Direction) hlt.Direction {
 	}
 }
 
-type Initiative struct {
-	Owner int
+type Fields struct {
+	Border     *FlowField
+	Initiative *FlowField
+	Threat     map[int]*FlowField
 }
 
-type BotMap struct {
-	Owner     int
-	GameMap   hlt.GameMap
-	Turn      int
-	Cells     [][]*Cell
-	PrevCells [][]*Cell
-	Moves     map[hlt.Location]hlt.Move
-	PrevMoves map[hlt.Location]hlt.Move
-	OwnedRows []bool
-	OwnedCols []bool
-}
-
-func NewBotMap(owner int, gameMap hlt.GameMap) BotMap {
-	var bm = BotMap{Owner: owner, GameMap: gameMap, Turn: 0}
-	return bm
-}
-
-func (bm *BotMap) updateMap(gameMap hlt.GameMap) {
-	bm.Turn = bm.Turn + 1
-	bm.PrevCells = bm.Cells
-	bm.Cells = make([][]*Cell, gameMap.Height)
-	bm.PrevMoves = bm.Moves
-	bm.Moves = make(map[hlt.Location]hlt.Move)
-
-	// init owned rows to true
-	bm.OwnedRows = make([]bool, gameMap.Height)
-	for r := 0; r < gameMap.Height; r++ {
-		bm.OwnedRows[r] = true
+// Assumes Bot Cell lists have been populated
+func NewFields(bot *Bot) *Fields {
+	var f = &Fields{
+		Threat: make(map[int]*FlowField),
 	}
-
-	// init owned cols to true
-	bm.OwnedCols = make([]bool, gameMap.Width)
-	for c := 0; c < gameMap.Width; c++ {
-		bm.OwnedCols[c] = true
+	f.Border = NewBorderField(bot.Owner, bot.OwnedCells, bot)
+	f.Initiative = NewInitiativeField(bot.Owner, bot.OwnedCells, bot)
+	for enemy, cells := range bot.EnemyCells {
+		f.Threat[enemy] = NewInitiativeField(enemy, cells, bot)
 	}
+	return f
+}
 
-	ownedBorders := NewStack()
-	enemyBorders := NewStack()
-	for y := 0; y < gameMap.Height; y++ {
-		bm.Cells[y] = make([]*Cell, gameMap.Width)
-		for x := 0; x < gameMap.Width; x++ {
-			var cell = NewCell(gameMap, x, y)
-			if cell.Site.Owner != bm.Owner {
-				if cell.Site.Owner != 0 && cell.isBorder() {
-					// Track enemby
-					cell.ThreatDistance = 0
-					cell.ThreatOwner = cell.Site.Owner
-					cell.ThreatStrength = cell.Site.Strength
-					enemyBorders.Push(cell)
-				}
-				bm.OwnedRows[y] = false
-				bm.OwnedCols[x] = false
-			} else if cell.isBorder() {
-				cell.BorderDistance = 0
-				ownedBorders.Push(cell)
-			}
-			bm.SetCell(cell)
+func (f *Fields) TopThreat(cell *Cell) *FlowField {
+	var topField *FlowField
+	for _, field := range f.Threat {
+		if topField == nil || field.Strength[cell] > topField.Strength[cell] {
+			topField = field
 		}
 	}
+	return topField
+}
 
-	// produce nearest border flow field
-	for ownedBorders.isNotEmpty() {
-		cell, _ := ownedBorders.Pop()
+func (f *Fields) String(cell *Cell) string {
+	if direction, ok := f.Border.Direction[cell]; ok {
+		if direction == hlt.STILL {
+			return fmt.Sprint(cell.Site.Owner)
+		} else {
+
+		}
+	}
+	return " "
+}
+
+type FlowField struct {
+	Direction map[*Cell]hlt.Direction
+	Distance  map[*Cell]int
+	Strength  map[*Cell]int
+}
+
+func NewFlowField(owner int) *FlowField {
+	return &FlowField{
+		Direction: make(map[*Cell]hlt.Direction),
+		Distance:  make(map[*Cell]int),
+		Strength:  make(map[*Cell]int),
+	}
+}
+
+// Direction to nearest owned border cell
+// Distance to nearest owned border cell
+// Strength of nearest owned border cell
+func NewBorderField(owner int, cells []*Cell, bot *Bot) *FlowField {
+	var field = NewFlowField(owner)
+	var stack = NewStack()
+	// push border cells onto the stack
+	for _, cell := range cells {
+		field.Direction[cell] = hlt.STILL
+		if cell.isBorder() {
+			field.Distance[cell] = 0
+			stack.Push(cell)
+		} else {
+			field.Distance[cell] = 999999
+		}
+	}
+	// produce nearest border flow field by working inward from border
+	for stack.isNotEmpty() {
+		cell, _ := stack.Pop()
+		var borderDistance = field.Distance[cell]
 		for _, direction := range hlt.CARDINALS {
-			otherCell := bm.GetCell(cell.Location, direction)
-			if otherCell.Site.Owner == bm.Owner {
-				if otherCell.BorderDistance > cell.BorderDistance+1 {
-					otherCell.BorderDirection = opposite(direction)
-					otherCell.BorderDistance = cell.BorderDistance + 1
-					bm.SetCell(otherCell)
-					ownedBorders.Push(otherCell)
+			otherCell := bot.GetCell(cell.Location, direction)
+			if otherCell.Site.Owner == owner {
+				if otherDist, ok := field.Distance[cell]; (ok && otherDist > borderDistance+1) || !ok {
+					field.Direction[otherCell] = opposite(direction)
+					field.Distance[otherCell] = borderDistance + 1
+					stack.Push(otherCell)
 				}
 			}
 		}
 	}
+	return field
+}
 
+// Direction of strongest threat
+// Distance to threat source
+// Strength of threat if it were to travel to this cell without reinforcements
+func NewInitiativeField(owner int, cells []*Cell, bot *Bot) *FlowField {
+	var field = NewFlowField(owner)
+	var stack = NewStack()
+	// push border cells onto the stack
+	for _, cell := range cells {
+		field.Direction[cell] = hlt.STILL
+		if cell.isBorder() {
+			field.Distance[cell] = 0
+			field.Strength[cell] = cell.Site.Strength
+			stack.Push(cell)
+		}
+	}
 	// produce strongest threat flow field
-	for enemyBorders.isNotEmpty() {
-		var cell, _ = enemyBorders.Pop()
+	for stack.isNotEmpty() {
+		cell, _ := stack.Pop()
+		// var borderDistance = field.Distance[cell]
 		for _, direction := range hlt.CARDINALS {
-			otherCell := bm.GetCell(cell.Location, direction)
-			if otherCell.Site.Owner != cell.ThreatOwner {
-				var newThreatStrength = cell.ThreatStrength - otherCell.Site.Strength
-				if newThreatStrength > 0 && newThreatStrength > otherCell.ThreatStrength {
-					otherCell.ThreatDirection = opposite(direction)
-					otherCell.ThreatDistance = cell.ThreatDistance + 1
-					otherCell.ThreatOwner = cell.ThreatOwner
-					otherCell.ThreatStrength = newThreatStrength
-					bm.SetCell(otherCell)
-					enemyBorders.Push(otherCell)
+			otherCell := bot.GetCell(cell.Location, direction)
+			if otherCell.Site.Owner != owner {
+				var remainingStrength = field.Strength[cell] - otherCell.Site.Strength
+				if remainingStrength > 0 {
+					previousStrength, ok := field.Strength[otherCell]
+					if !ok || remainingStrength > previousStrength {
+						field.Direction[otherCell] = opposite(direction)
+						field.Distance[otherCell] = field.Distance[cell] + 1
+						field.Strength[otherCell] = remainingStrength
+						stack.Push(otherCell)
+					}
 				}
 			}
 		}
 	}
+	return field
+}
 
-	bm.logMap()
+type Agent struct {
+}
+
+type Bot struct {
+	Owner      int
+	Agents     map[int]*Agent
+	GameMap    *hlt.GameMap
+	Turn       int
+	Fields     *Fields
+	Cells      [][]*Cell
+	OwnedCells []*Cell
+	EmptyCells []*Cell
+	EnemyCells map[int][]*Cell
+}
+
+func NewBot(owner int) *Bot {
+	return &Bot{
+		Owner:  owner,
+		Agents: make(map[int]*Agent),
+		Turn:   0,
+	}
+}
+
+func (bot *Bot) UpdateMap(gameMap *hlt.GameMap) {
+	bot.GameMap = gameMap
+	bot.Turn = bot.Turn + 1
+	bot.Cells = make([][]*Cell, gameMap.Height)
+	bot.OwnedCells = make([]*Cell, 1)
+	bot.EmptyCells = make([]*Cell, 1)
+	bot.EnemyCells = make(map[int][]*Cell)
+
+	// Sorts Map Cells into "owned", "empty", and "enemy" lists
+	for y := 0; y < gameMap.Height; y++ {
+		bot.Cells[y] = make([]*Cell, gameMap.Width)
+		for x := 0; x < gameMap.Width; x++ {
+			var cell = NewCell(*gameMap, x, y)
+			bot.SetCell(cell)
+		}
+	}
+
+	bot.Fields = NewFields(bot)
+
+	bot.logBot()
 
 	// Threat that overlaps our borders produce Assistence Requesting Agents
 	// and Assitence Lending Agents.
@@ -188,38 +257,65 @@ func (bm *BotMap) updateMap(gameMap hlt.GameMap) {
 	// and begin moving to assist.  Agents die as call is answered. :D
 }
 
-func (bm *BotMap) logMap() {
+func (bot *Bot) Height() int {
+	return bot.GameMap.Height
+}
+
+func (bot *Bot) Width() int {
+	return bot.GameMap.Width
+}
+
+func (bot *Bot) GetCell(loc hlt.Location, direction hlt.Direction) *Cell {
+	loc = bot.GameMap.GetLocation(loc, direction)
+	return bot.Cells[loc.Y][loc.X]
+}
+
+func (bot *Bot) SetCell(cell *Cell) {
+	bot.Cells[cell.Y][cell.X] = cell
+	var cellOwner = cell.Site.Owner
+	if cellOwner > 0 {
+		if cellOwner == bot.Owner {
+			bot.OwnedCells = append(bot.OwnedCells, cell)
+		} else {
+			bot.EnemyCells[cellOwner] = append(bot.EnemyCells[cellOwner], cell)
+		}
+	} else {
+		bot.EmptyCells = append(bot.EmptyCells, cell)
+	}
+}
+
+func (bot *Bot) logBot() {
 	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	var mapStr = fmt.Sprintf("Map %d:\n", bm.Turn)
-	for y := 0; y < bm.GameMap.Height; y++ {
+	var mapStr = fmt.Sprintf("Map %d:\n", bot.Turn)
+	for y := 0; y < bot.GameMap.Height; y++ {
 		var str = ""
-		for x := 0; x < bm.GameMap.Width; x++ {
-			var cell = bm.Cells[y][x]
+		for x := 0; x < bot.GameMap.Width; x++ {
+			var cell = bot.Cells[y][x]
 			if cell.isBorder() && cell.Site.Owner > 0 {
 				str = fmt.Sprintf("%v %d ", str, cell.Site.Owner)
-			} else if cell.Site.Owner > 0 && cell.Site.Owner != cell.ThreatOwner {
-				str = fmt.Sprintf("%v : ", str)
+				// } else if cell.Site.Owner > 0 && cell.Site.Owner != cell.ThreatOwner {
+				// 	str = fmt.Sprintf("%v : ", str)
 			} else if cell.Site.Owner > 0 {
 				str = fmt.Sprintf("%v . ", str)
-			} else if cell.ThreatStrength > 0 {
-				var dirStr string
-				switch opposite(cell.ThreatDirection) {
-				case hlt.NORTH:
-					dirStr = "^"
-				case hlt.SOUTH:
-					dirStr = "v"
-				case hlt.WEST:
-					dirStr = "<"
-				case hlt.EAST:
-					dirStr = ">"
-				default:
-					dirStr = "x"
-				}
-				str = fmt.Sprintf("%v %v ", str, dirStr)
+				// } else if cell.ThreatStrength > 0 {
+				// 	var dirStr string
+				// 	switch opposite(cell.ThreatDirection) {
+				// 	case hlt.NORTH:
+				// 		dirStr = "^"
+				// 	case hlt.SOUTH:
+				// 		dirStr = "v"
+				// 	case hlt.WEST:
+				// 		dirStr = "<"
+				// 	case hlt.EAST:
+				// 		dirStr = ">"
+				// 	default:
+				// 		dirStr = "x"
+				// 	}
+				// 	str = fmt.Sprintf("%v %v ", str, dirStr)
 			} else {
 				str = fmt.Sprintf("%v   ", str)
 			}
@@ -231,15 +327,6 @@ func (bm *BotMap) logMap() {
 	}
 }
 
-func (bm *BotMap) GetCell(loc hlt.Location, direction hlt.Direction) *Cell {
-	loc = bm.GameMap.GetLocation(loc, direction)
-	return bm.Cells[loc.Y][loc.X]
-}
-
-func (bm *BotMap) SetCell(cell *Cell) {
-	bm.Cells[cell.Y][cell.X] = cell
-}
-
 type Cell struct {
 	GameMap hlt.GameMap
 	// Provided Cell data
@@ -247,31 +334,17 @@ type Cell struct {
 	X        int
 	Location hlt.Location
 	Site     hlt.Site
-	// For cells belonging to the Owner, flow field to the border
-	BorderDirection hlt.Direction
-	BorderDistance  int
-	// Field of threat radiating from edge of enemy borders
-	ThreatDirection hlt.Direction
-	ThreatDistance  int
-	ThreatOwner     int
-	ThreatStrength  int
 }
 
 func NewCell(gameMap hlt.GameMap, x int, y int) *Cell {
 	location := hlt.NewLocation(x, y)
 	site := gameMap.GetSite(location, hlt.STILL)
 	return &Cell{
-		GameMap:         gameMap,
-		Y:               y,
-		X:               x,
-		Location:        location,
-		Site:            site,
-		BorderDirection: hlt.STILL,
-		BorderDistance:  999999,
-		ThreatDirection: hlt.STILL,
-		ThreatDistance:  999999,
-		ThreatOwner:     site.Owner,
-		ThreatStrength:  0,
+		GameMap:  gameMap,
+		Y:        y,
+		X:        x,
+		Location: location,
+		Site:     site,
 	}
 }
 
@@ -295,35 +368,19 @@ func (c *Cell) isBorder() bool {
 }
 
 func (c *Cell) String() string {
-	return fmt.Sprintf("Cell(x: %d, y: %d, dist:%d)", c.X, c.Y, c.BorderDistance)
-}
-
-// [ ][ ][ ][ ][ ]
-// [ ][ ][N][ ][ ]
-// [ ][W][x][E][ ]
-// [ ][ ][S][ ][ ]
-// [ ][ ][ ][ ][ ]
-type Need struct {
-	dir hlt.Direction
-	str int
-}
-
-func NewNeed(dir hlt.Direction, str int) Need {
-	return Need{
-		dir: dir,
-		str: str,
-	}
+	return fmt.Sprintf("Cell(x: %d, y: %d, owner:%d)", c.X, c.Y, c.Site.Owner)
 }
 
 func main() {
 	conn, gameMap := hlt.NewConnection("BrevBot")
-	botMap := NewBotMap(conn.PlayerTag, gameMap)
+	bot := NewBot(conn.PlayerTag)
+	bot.UpdateMap(&gameMap)
 	for {
 		var moves hlt.MoveSet
 		gameMap = conn.GetFrame()
 
 		startTime := time.Now()
-		botMap.updateMap(gameMap)
+		bot.UpdateMap(&gameMap)
 		for y := 0; y < gameMap.Height; y++ {
 			for x := 0; x < gameMap.Width; x++ {
 				loc := hlt.NewLocation(x, y)
