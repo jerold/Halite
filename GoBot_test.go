2d21146b1350eaee1796d478defb4726edf92dcc
jerold <jerold.albertson@gmail.com>
2016-12-15 00:06:18 -0700

    failed test found

--- GoBot_test.go
+++ GoBot_test.go
@@ -15,27 +15,57 @@ func setSite(owner, production, strength int, site *hlt.Site) {
 // Assumes 2D array containing arrays of 3 elements [Owner,Production,Strength]
 func MockGameBoard(owner, production, strength, width, height int) hlt.GameMap {
 	m := hlt.NewGameMap(width, height)
-	for y, _ := range m.Contents {
-		for x, _ := range m.Contents[y] {
+	for y := range m.Contents {
+		for x := range m.Contents[y] {
 			setSite(owner, production, strength, &m.Contents[y][x])
 		}
 	}
 	return m
 }
 
-func TestCells(t *testing.T) {
-
+func TestCellsSimulation1(t *testing.T) {
 	m := MockGameBoard(0, 0, 0, 5, 5)
-	setSite(1, 1, 15, &m.Contents[2][1])
-	setSite(1, 1, 15, &m.Contents[1][2])
-	setSite(1, 1, 15, &m.Contents[2][3])
-	setSite(2, 1, 10, &m.Contents[3][2])
+	setSite(1, 0, 15, &m.Contents[1][1])
+	setSite(1, 0, 15, &m.Contents[1][3])
+	setSite(1, 0, 15, &m.Contents[2][1])
+	setSite(1, 0, 15, &m.Contents[1][2])
+	setSite(1, 0, 15, &m.Contents[2][3])
+	setSite(2, 0, 10, &m.Contents[3][2])
 
 	cells := NewCells(1, 1, 3, 3, m)
 	fmt.Println(cells)
 	moves := hlt.MoveSet{}
-	moves = append(moves, hlt.Move{hlt.Location{3, 2}, hlt.NORTH})
-	moves = append(moves, hlt.Move{hlt.Location{1, 2}, hlt.SOUTH})
+	moves = append(moves, hlt.Move{Location: hlt.Location{X: 2, Y: 3}, Direction: hlt.NORTH})
+	moves = append(moves, hlt.Move{Location: hlt.Location{X: 2, Y: 1}, Direction: hlt.SOUTH})
+	newCells := cells.Simulate(moves)
+	fmt.Println("\"", newCells, "\"")
+}
+
+func TestCellsSimulation2(t *testing.T) {
+	m := MockGameBoard(0, 0, 0, 5, 5)
+	setSite(1, 0, 15, &m.Contents[2][1])
+	setSite(1, 0, 15, &m.Contents[1][2])
+	setSite(1, 0, 15, &m.Contents[2][3])
+	setSite(2, 0, 10, &m.Contents[3][2])
+
+	cells := NewCells(1, 1, 3, 3, m)
+	fmt.Println(cells)
+	moves := hlt.MoveSet{}
+	moves = append(moves, hlt.Move{Location: hlt.Location{X: 2, Y: 3}, Direction: hlt.NORTH})
+	newCells := cells.Simulate(moves)
+	fmt.Println(newCells)
+}
+
+func TestCellsSimulation3(t *testing.T) {
+	m := MockGameBoard(0, 0, 0, 2, 2)
+	setSite(1, 0, 15, &m.Contents[0][0])
+	setSite(2, 0, 15, &m.Contents[1][1])
+
+	cells := NewCells(0, 0, 2, 2, m)
+	fmt.Println(cells)
+	moves := hlt.MoveSet{}
+	moves = append(moves, hlt.Move{Location: hlt.Location{X: 0, Y: 0}, Direction: hlt.EAST})
+	moves = append(moves, hlt.Move{Location: hlt.Location{X: 1, Y: 1}, Direction: hlt.WEST})
 	newCells := cells.Simulate(moves)
 	fmt.Println(newCells)
 }
